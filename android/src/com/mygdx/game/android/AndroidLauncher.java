60ba9869fdf38bd6c9375e8f505416cc7358a035
jndm <jondemm@gmail.com>
2015-03-30 23:16:12 +0300

    Added some initial gamehandling classes

--- android/src/com/mygdx/game/android/AndroidLauncher.java
+++ android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mygdx.game.Main;
+import com.mygdx.game.Game;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
@@ -13,6 +13,6 @@ public class AndroidLauncher extends AndroidApplication {
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		config.useAccelerometer = false;
 	    config.useCompass = false;
-		initialize(new Main(), config);
+		initialize(new Game(), config);
 	}
 }
